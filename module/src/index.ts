0c05b301f6bb5f82c72ed33ff724df2352f8c9f4
Mst. Nusrat Jahan Joya <nusratjahanjoya707@gmail.com>
2025-05-08 22:42:29 +0600

    problem8

--- module/src/index.ts
+++ module/src/index.ts
@@ -47,33 +47,60 @@ class Car extends Vehicle {
 }
 const myCar = new Car("Toyota", 2020, "Corolla");
 
-
 function processValue(value: string | number): number {
-    if (typeof value === "string") {
-        return value.length;
-    }else{
-        return value * 2;
-    }
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
 }
 
 interface Product {
-    name: string;
-    price: number;
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) {
+    return null;
+  } else {
+    return products.reduce((maxProduct, currentProduct) =>
+      currentProduct.price > maxProduct.price ? currentProduct : maxProduct
+    );
   }
-  
-  function getMostExpensiveProduct(products: Product[]): Product | null {
-    if (products.length === 0) {
-        return null;
-    }else{
-        return products.reduce((maxProduct, currentProduct) => 
-        currentProduct.price > maxProduct.price ? currentProduct : maxProduct );
-    }
+}
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 },
+];
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  } else {
+    return "Weekday ";
   }
-  const products = [
-    { name: "Pen", price: 10 },
-    { name: "Notebook", price: 25 },
-    { name: "Bag", price: 50 }
-  ];
-  
-  console.log(getMostExpensiveProduct(products));  
-  // Output: { name: "Bag", price: 50 }
+}
+
+async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (n < 0) {
+                reject(new Error("Negative number not allowed"));
+            }else{
+                resolve(n * n);
+            }
+        },1000);
+    });
+}
