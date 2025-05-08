ec96163fcf5986eb23dc15e688b7b36b3f891a07
Mst. Nusrat Jahan Joya <nusratjahanjoya707@gmail.com>
2025-05-08 22:13:33 +0600

    problem3

--- module/src/index.ts
+++ module/src/index.ts
@@ -1,17 +1,51 @@
-function formatString(input: string, toUpper: boolean = true) : string {
-    return toUpper ? input.toUpperCase() : input.toLowerCase();
+function formatString(input: string, toUpper: boolean = true): string {
+  return toUpper ? input.toUpperCase() : input.toLowerCase();
 }
 
-function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
-    return items.filter(item => item.rating >= 4);
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
 }
 const books = [
-    { title: "Book A", rating: 4.5 },
-    { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 }
-  ];
-  
-  filterByRating(books); 
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
 
-  
-  
+filterByRating(books);
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  getInfo(): string {
+    return `Make : ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel(): string {
+    return `Model : ${this.model}`;
+  }
+}
+const myCar = new Car("Toyota", 2020, "Corolla");
+
+
+
