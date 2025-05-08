df04080663a94b6524a882d1b0edacdfa3fff86a
Mst. Nusrat Jahan Joya <nusratjahanjoya707@gmail.com>
2025-05-08 22:23:34 +0600

    problrm6

--- module/src/index.ts
+++ module/src/index.ts
@@ -48,4 +48,32 @@ class Car extends Vehicle {
 const myCar = new Car("Toyota", 2020, "Corolla");
 
 
+function processValue(value: string | number): number {
+    if (typeof value === "string") {
+        return value.length;
+    }else{
+        return value * 2;
+    }
+}
 
+interface Product {
+    name: string;
+    price: number;
+  }
+  
+  function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+        return null;
+    }else{
+        return products.reduce((maxProduct, currentProduct) => 
+        currentProduct.price > maxProduct.price ? currentProduct : maxProduct );
+    }
+  }
+  const products = [
+    { name: "Pen", price: 10 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 }
+  ];
+  
+  console.log(getMostExpensiveProduct(products));  
+  // Output: { name: "Bag", price: 50 }
