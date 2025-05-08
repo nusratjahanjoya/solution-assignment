bb913eb1a270c37457bde88e0c5c822692295c87
Mst. Nusrat Jahan Joya <nusratjahanjoya707@gmail.com>
2025-05-08 21:17:53 +0600

    problem2

--- module/src/index.ts
+++ module/src/index.ts
@@ -2,4 +2,16 @@ function formatString(input: string, toUpper: boolean = true) : string {
     return toUpper ? input.toUpperCase() : input.toLowerCase();
 }
 
-console.log(formatString("Hello"));
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+  ];
+  
+  filterByRating(books); 
+
+  
+  
